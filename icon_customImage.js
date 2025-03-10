3e9de4a82765a104a54d7ff77f80972d29e7c26e
hristishh <hristina1504fet@gmail.com>
2025-03-11 01:34:51 +0300

    Update icon_customImage.js

--- icon_customImage.js
+++ icon_customImage.js
@@ -17,7 +17,8 @@ function init() {
         degt: { src: 'degt.png', width: 100, height: 100 },
         passion: { src: 'passion.png', width: 100, height: 100 },
         passion2: { src: 'passion2.png', width: 100, height: 100 },
-        prech: { src: 'prech.png', width: 100, height: 100 }
+        prech: { src: 'prech.png', width: 100, height: 100 },
+        mir: { src: 'mir.png', width: 100, height: 100 }
     };
 
     // Данные меток
@@ -26,7 +27,18 @@ function init() {
         { coordinates: [55.768532, 37.604978], imageKey: 'degt' },
         { coordinates: [55.764987, 37.608212], imageKey: 'passion' },
         { coordinates: [55.767053, 37.611859], imageKey: 'passion2' },
-        { coordinates: [55.744064, 37.590856], imageKey: 'prech' }
+        { coordinates: [55.744064, 37.590856], imageKey: 'prech' },
+        { coordinates: [55.781793, 37.634290], imageKey: 'mir' }
+    ];
+
+     // Данные меток достопримечательностей
+    const attractionsData = [
+        { coordinates: [55.747224, 37.605240], title: 'Museum', imageSrc: 'museum.png' },
+        { coordinates: [55.753544, 37.621202], title: 'Red Square', imageSrc: 'square.png' },
+        { coordinates: [55.760135, 37.624957], title: 'Central Children_s Store', imageSrc: 'cdm.png' },
+        { coordinates: [55.760056, 37.618672], title: 'Bolshoi Theatre', imageSrc: 'theatre.png' },
+        { coordinates: [55.731474, 37.603431], title: 'Gorky Park', imageSrc: 'park.png' },
+        { coordinates: [55.741556, 37.620028], title: 'Gallery', imageSrc: 'gallery.png' }
     ];
 
     placemarksData.forEach(data => {
@@ -38,7 +50,7 @@ function init() {
             iconContentLayout: MyIconContentLayout,
             iconLayout: 'default#imageWithContent',
             iconImageHref: 'images.png',
-            iconImageSize: [30, 50],
+            iconImageSize: [40, 60],
             iconImageOffset: [-5, -38]
         });
 
@@ -47,14 +59,14 @@ function init() {
         myPlacemark.events
             .add('mouseenter', function (e) {
                 e.get('target').options.set('iconLayout', 'default#imageWithContent');
-                e.get('target').options.set('iconImageSize', [240, 144]);
+                e.get('target').options.set('iconImageSize', [200, 120]);
                 e.get('target').options.set('iconImageOffset', [-images[data.imageKey].width, -images[data.imageKey].height]);
-                e.get('target').options.set('iconContentOffset', [20, 15]);
+                e.get('target').options.set('iconContentOffset', [-120, -200]);
                 e.get('target').options.set('iconImageHref', images[data.imageKey].src);
             })
             .add('mouseleave', function (e) {
                 e.get('target').options.set('iconLayout', 'default#imageWithContent');
-                e.get('target').options.set('iconImageSize', [30, 50]);
+                e.get('target').options.set('iconImageSize', [40, 60]);
                 e.get('target').options.set('iconImageOffset', [-5, -38]);
                 e.get('target').options.set('iconImageHref', 'images.png');
             })
@@ -82,6 +94,20 @@ function init() {
                 modal.appendChild(img);
                 document.body.appendChild(modal);
             });
+    });
+    // Добавление меток достопримечательностей с индивидуальными изображениями
+    attractionsData.forEach(attraction => {
+        const attractionPlacemark = new ymaps.Placemark(attraction.coordinates, {
+            hintContent: attraction.title,
+            balloonContent: attraction.title,
+        }, {
+            iconLayout: 'default#image',
+            iconImageHref: attraction.imageSrc,
+            iconImageSize: [100, 55],
+            iconImageOffset: [-15, -30] // Центрирование по нижнему центру
+        });
+
+        myMap.geoObjects.add(attractionPlacemark);
     });
 }
 
